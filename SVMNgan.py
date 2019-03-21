66daeeb20b8356b2017055dd1de21166cc00b6d7
plee98 <nganltp98@gmail.com>
2019-03-21 17:27:37 +0700

    train successfully, bug evalute

--- SVMNgan.py
+++ SVMNgan.py
@@ -12,6 +12,7 @@ import cv2
 import os
 import itertools as it
 from contextlib import contextmanager
+import random
 
 SZ = 20
 
@@ -52,6 +53,44 @@ def get_hog() :
     return hog
     affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
 
+def svmInit(C=12.5, gamma=0.50625):
+  model = cv2.ml.SVM_create()
+  model.setGamma(gamma)
+  model.setC(C)
+  model.setKernel(cv2.ml.SVM_RBF)
+  model.setType(cv2.ml.SVM_C_SVC)
+  
+  return model
+
+def svmTrain(model, samples, responses):
+  model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+  model.save("fire_svm_model.yml")
+  return model
+
+def svmPredict(model, samples):
+  return model.predict(samples)[1].ravel()
+
+def svmEvaluate(model, digits, samples, labels):
+    predictions = svmPredict(model, samples)
+    # print(labels)
+    accuracy = (labels == predictions).mean()
+    print('Percentage Accuracy: %.2f %%' % (accuracy*100))
+
+    # confusion = np.zeros((10, 10), np.int32)
+    # for i, j in zip(labels, predictions):
+    #     confusion[int(i), int(j)] += 1
+    # print('confusion matrix:')
+    # print(confusion)
+
+    # vis = []
+    # for img, flag in zip(digits, predictions == labels):
+    #     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    #     if not flag:
+    #         img[...,:2] = 0
+        
+    #     vis.append(img)
+    # return mosaic(25, vis)
+
 # winSize = (64,64)
 # blockSize = (16,16)
 # blockStride = (8,8)
@@ -65,11 +104,11 @@ def get_hog() :
 # nlevels = 64
 
 if __name__ == '__main__':
-    file = open("D:\\TrainSVM-OpenCV\\savePathFireImg.txt","r+") # r = read
+    file = open("D:\\work\\GIT\\Fire-Detection\\savePathFireImg.txt","r+") # r = read
     contents = file.readlines()
     labels = []
+    data = []
     hog_descriptors = []
-    img = []
     count = 0
     hog = get_hog()
     # hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
@@ -77,7 +116,7 @@ if __name__ == '__main__':
     for pathImg in contents:
         # gan nhan labels
         count = count + 1
-        if(count < 296 ):
+        if(count < 201 ):
             labels.append(1)
         else:
             labels.append(0)
@@ -86,7 +125,7 @@ if __name__ == '__main__':
         # print(pathImg)
         img = cv2.imread(pathImg)
 
-
+        data.append(img)
         # compute(img[, winStride[, padding[, locations]]]) -> descriptors
         winStride = (8,8)
         padding = (8,8)
@@ -101,11 +140,38 @@ if __name__ == '__main__':
 
         # exit()
         
+        
     # hist.save("hog.xml")
 
     hog_descriptors = np.squeeze(hog_descriptors)
-    print(type(hog_descriptors))
-    # print(labels)
+
+    # print('Shuffle data ... ')
+    # # Shuffle data
+    # shuffle_order = list(range(len(labels)))
+    # random.shuffle(shuffle_order)
+    # data = data[shuffle_order]
+    # labels = labels[shuffle_orther]
+
+    # rand=np.random.RandomState(10)
+    # shuffle = rand.permutation(len(labels))
+    # data, labels = data[shuffle], labels[shuffle]
+
+    print('Spliting data into training (80%) and test set (20%)... ')
+    train_n=int(0.8*len(hog_descriptors))
+    data_train, data_test = np.split(data, [train_n])
+    hog_descriptors_train, hog_descriptors_test = np.split(hog_descriptors, [train_n])
+    labels_train, labels_test = np.split(labels, [train_n])
+
+
+    print('Training SVM model ...')
+    model = svmInit()
+    svmTrain(model, hog_descriptors_train, labels_train)
+
+    print('Evaluating model ... ')
+    svmEvaluate(model, data_test, hog_descriptors_test, labels_test)
+
+    # print(type(hog_descriptors))
+    print(labels)
     # print(len(labels))
 
     file.close()
